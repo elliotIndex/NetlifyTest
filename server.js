51dcc436a38c1a399f432e7967e3b1a200cf2be8
Elliot Plant <elliotaplant@gmail.com>
2017-05-13 17:21:17 -0700

    Beautify

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-var config = require('./config.json');
-var netlify = require('netlify');
+const config = require('./config.json');
+const netlify = require('netlify');
 
-var client = netlify.createClient({access_token: config.access_token});
+const client = netlify.createClient({
+  access_token: config.access_token
+});
 
 client.sites()
-.then(sites => { console.log('sites before', sites.map(site => site.name)); })
-.then(() => client.createSite({ name: 'elliot-test-site-2', }))
-.then(site => site.createDeploy({dir: './dist'}))
-.then(deploy => deploy.waitForReady())
-.then(something => { console.log('Deploy is done: ', something); })
-.catch(error => { console.log('Error!'); console.error(error); })
-.then(() => client.sites())
-.then(sites => { console.log('sites after', sites.map(site => site.name)); });
+  .then(sites => {
+    console.log('sites before', sites.map(site => site.name));
+  })
+  .then(() => client.createSite({
+    name: 'elliot-test-site-2'
+  }))
+  .then(site => site.createDeploy({
+    dir: './dist'
+  }))
+  .then(deploy => deploy.waitForReady())
+  .then(something => {
+    console.log('Deploy is done: ', something);
+  })
+  .catch(error => {
+    console.log('Error!');
+    console.error(error);
+  })
+  .then(() => client.sites())
+  .then(sites => {
+    console.log('sites after', sites.map(site => site.name));
+  });
